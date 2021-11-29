8f9c4cf1768a8f751c024989dcdbd1e939184915
Jolle <37653945+JohanDraaby@users.noreply.github.com>
2021-11-29 12:56:34 +0100

    Added game to branch

---
+++ FiskErFremtidenGameDemo/Assets/Scripts/Food.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    private string _name;
+
+    public string Name
+    {
+        get { return _name; }
+    }
+
+
+
+}
