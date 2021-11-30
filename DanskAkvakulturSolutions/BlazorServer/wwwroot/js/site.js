1e49d4c8a915234d7d85f7d567937da8926fed84
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2021-11-30 19:13:44 +0100

    Deleted website files due to merge error.

--- DanskAkvakulturSolutions/BlazorServer/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-﻿var GLOBAL = {};
-GLOBAL.DotNetReference = null;
-GLOBAL.SetDotnetReference = function (pDotNetReference) {
-    GLOBAL.DotNetReference = pDotNetReference;
-};
-
-window.setDotNetReference = function (objRef) {
-    GLOBAL.SetDotnetReference(objRef);
-};
