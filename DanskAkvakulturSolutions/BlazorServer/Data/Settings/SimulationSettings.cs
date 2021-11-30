1fdf505bc09f84a0c8ee9f00efe43c411fbdcc18
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2021-11-30 19:20:13 +0100

    Updated main with corrupt branch updates.

---
+++ DanskAkvakulturSolutions/BlazorServer/Data/Settings/SimulationSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Data.Settings
+{
+    public class SimulationSettings : ISimulationSettings
+    {
+        public string RelativeUri { get; set; }
+    }
+}
