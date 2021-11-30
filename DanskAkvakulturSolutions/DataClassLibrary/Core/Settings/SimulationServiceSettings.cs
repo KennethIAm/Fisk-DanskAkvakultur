e8ebf75c8363d48a6d3b4fb14311014c58c6c406
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2021-11-30 12:34:58 +0100

    Refactored the Web-Dev/base
    - Moved the implementation of the branch into the DataClassLibrary/base branch.

---
+++ DanskAkvakulturSolutions/DataClassLibrary/Core/Settings/SimulationServiceSettings.cs
@@ -0,0 +1,12 @@
+﻿using DataClassLibrary.Core.Settings.Interfaces;
+using System;
+
+namespace DataClassLibrary.Core.Settings
+{
+    public class SimulationServiceSettings : ISimulationSettings
+    {
+        private string RelativeUri { get; set; }
+
+        public Uri AbsoluteUri { get; set; }
+    }
+}
---
+++ DanskAkvakulturSolutions/DataClassLibrary/Simulation/Services/Interfaces/ISimulationService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace DataClassLibrary.Simulation.Services.Interfaces
+{
+    public interface ISimulationService
+    {
+        ///// <summary>
+        ///// Returns the state of the connection. True if connected, otherwise false.
+        ///// </summary>
+        public bool IsConnected { get; }
+
+        /// <summary>
+        /// Send message to the Hub.
+        /// </summary>
+        /// <returns>A task representing the asynchronous process that sends the message to the hub.</returns>
+        Task SendMessageAsync();
+
+        /// <summary>
+        /// Connects directly to the base of a Hub. This should only be called once.
+        /// </summary>
+        /// <returns>A task representing the asynchronous process which connects to a hub.</returns>
+        Task ConnectAsync();
+    }
+}
